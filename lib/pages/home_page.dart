635d09d35648f457b2060defb0647d6e307bf4be
Gabriel de Bem <gabrieldebem72@gmail.com>
2023-08-13 10:55:20 -0300

    initial commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:sofia_app/bindings/route.binding.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Sofia App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Container(
+                color: Theme.of(context).colorScheme.primary,
+                child: Center(
+                  child: TextButton(
+                    style: ButtonStyle(
+                      elevation: MaterialStateProperty.all<double>(10.0),
+                      shadowColor: MaterialStateProperty.all<Color>(
+                        Colors.black,
+                      ),
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                        Colors.white,
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.pushNamed(context, RouteBinding.signup);
+                    },
+                    child: Text(
+                      'Quero criar uma conta',
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Center(
+                child: TextButton(
+                  style: ButtonStyle(
+                    elevation: MaterialStateProperty.all<double>(10.0),
+                    shadowColor: MaterialStateProperty.all<Color>(
+                      Colors.black,
+                    ),
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        Theme.of(context).colorScheme.primary),
+                  ),
+                  onPressed: () {
+                    Navigator.pushNamed(context, RouteBinding.login);
+                  },
+                  child: Text(
+                    'Já tenho uma conta',
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.onPrimary,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
