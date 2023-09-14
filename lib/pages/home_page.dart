20e4d8351be3a454e4fe98f66f6c6d2547c9f9fa
Gabriel de Bem <gabrieldebem72@gmail.com>
2023-09-14 19:00:43 -0300

    fix transaction list

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sofia_app/bindings/routes.dart';
-import 'package:sofia_app/components/spends_list.dart';
 import 'package:sofia_app/controllers/home.controller.dart';
 import 'package:sofia_app/components/create_spend_dialog.dart';
 import 'package:sofia_app/pages/profile_page.dart';
@@ -26,7 +25,9 @@ class _HomePageState extends State<HomePage> {
           actions: [
             IconButton(
               onPressed: () {
-                Get.toNamed(Routes.profile, arguments: ProfilePageArguments(user: _homeController.user));
+                Get.toNamed(Routes.profile,
+                    arguments:
+                        ProfilePageArguments(user: _homeController.user));
               },
               icon: const Icon(Icons.person),
             ),
@@ -41,27 +42,70 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
         body: RefreshIndicator(
-          onRefresh: () async {
-            await _homeController.fetchSpends();
-          },
-          child: SingleChildScrollView(
-            physics: const AlwaysScrollableScrollPhysics(),
-            child: Column(
-              children: [
-                const SizedBox(height: 20),
-                const Text(
-                  "Seus Gastos",
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.w500,
+            onRefresh: () async {
+              await _homeController.fetchSpends();
+            },
+            child: SingleChildScrollView(
+              physics: const AlwaysScrollableScrollPhysics(),
+              child: Column(
+                children: [
+                  const SizedBox(height: 20),
+                  const Text(
+                    "Seus Gastos",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w500,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 20),
-                SpendList(_homeController.spends),
-              ],
-            ),
-          )
-        ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    height: 600,
+                    child: _homeController.spends.isEmpty
+                        ? Center(
+                            child: Text(
+                              'Nenhum gasto cadastrado',
+                              style: Theme.of(context).textTheme.headlineMedium,
+                            ),
+                          )
+                        : ListView.builder(
+                            padding: const EdgeInsets.all(8),
+                            itemCount: _homeController.spends.length,
+                            itemBuilder: (ctx, index) {
+                              return ListTile(
+                                isThreeLine: true,
+                                visualDensity: const VisualDensity(vertical: 2),
+                                title: Text(
+                                  _homeController.spends[index].category
+                                      .toUpperCase(),
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                subtitle: Text(
+                                  _homeController
+                                          .spends[index].description.isEmpty
+                                      ? "Sem descrição"
+                                      : _homeController
+                                          .spends[index].description,
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                  ),
+                                ),
+                                trailing: Text(
+                                  "R\$ ${_homeController.spends[index].amount.toString()}",
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                  ),
+                ],
+              ),
+            )),
         floatingActionButton: FloatingActionButton(
           shape: const CircleBorder(),
           onPressed: () async {
